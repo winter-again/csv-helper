93d6ff9e8e83e18e21829a26d516a1a6a49b4434
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2024-06-01 10:58:04 -0400

    Figuring out how to test command output

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,78 @@
+from pathlib import Path
+
+# from polars.testing import assert_frame_equal
+import polars as pl
 from csv_helper.main import app
 from typer.testing import CliRunner
 
 runner = CliRunner()
 
 
-def test_app():
+def test_preview_cmd():
     result = runner.invoke(
-        app, ["preview", "./data/county_week_covid_v2.csv", "-n", "15"]
+        app, ["preview", "./tests/data/test_impute_data.csv", "-n", "15"]
     )
+
+    assert Path("./tests/data/test_impute_data.csv").is_file()
     assert result.exit_code == 0
+    assert "File: tests/data/test_impute_data.csv" in result.stdout
+
+
+def test_impute_result(tmp_path):
+    tmp_dir = tmp_path / "csv-helper"
+    tmp_dir.mkdir()
+    tmp_file = tmp_dir / "test_impute_output.csv"
+    fill_range = (1, 5)
+
+    result = runner.invoke(
+        app,
+        [
+            "impute",
+            "./tests/data/test_impute_data.csv",
+            str(tmp_file),
+            "-c",
+            "cases",
+            "-f",
+            f"<={fill_range[1]}",
+            "-r",
+            f"{fill_range[0]},{fill_range[1]}",
+            "-s",
+            "8",
+        ],
+    )
+
+    assert result.exit_code == 0
+    assert tmp_file.is_file()
+
+    df_in = (
+        pl.read_csv("./tests/data/test_impute_data.csv", infer_schema_length=0)
+        .with_row_index(name="id")
+        .select(["id", "cases"])
+    )
+    df_out = (
+        pl.read_csv(tmp_file, infer_schema_length=0)
+        .with_row_index(name="id")
+        .select(["id", "cases"])
+    )
+    df = df_in.join(df_out, on="id", how="inner", suffix="_imputed").filter(
+        pl.col("cases") == f"<={fill_range[1]}"
+    )
+
+    assert (
+        df.select("cases_imputed").to_series().str.contains(f"<={fill_range[1]}").all()
+        is False
+    )
+    assert (
+        df.select("cases_imputed")
+        .cast(pl.Int64)
+        .filter(pl.col("cases_imputed") > fill_range[1])
+        .height
+        == 0
+    )
+    assert (
+        df.select("cases_imputed")
+        .cast(pl.Int64)
+        .filter(pl.col("cases_imputed") < fill_range[0])
+        .height
+        == 0
+    )
