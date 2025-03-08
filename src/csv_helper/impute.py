4f5fa81cad55a111a102bd76bc41bbd95c6ab999
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2025-03-08 14:27:55 -0500

    Draft of lib funcs

--- src/csv_helper/impute.py
+++ src/csv_helper/impute.py
@@ -1,28 +1,14 @@
-from typing import NamedTuple, TypeAlias
+from typing import NamedTuple
 
 import numpy as np
 import polars as pl
-
-PolarsNumericType: TypeAlias = (
-    pl.Decimal
-    | pl.Float32
-    | pl.Float64
-    | pl.Int8
-    | pl.Int16
-    | pl.Int32
-    | pl.Int64
-    | pl.Int128
-    | pl.UInt8
-    | pl.UInt16
-    | pl.UInt32
-    | pl.UInt64
-)
+from polars._typing import PolarsDataType
 
 
 def check(df: pl.DataFrame, fill_cols: list[str], fill_flag: str) -> pl.DataFrame:
     """
-    Return dataframe with counts and proportion of instances of fill_flag in each of
-    the given fill_cols
+    Return dataframe with counts and proportion of instances of `fill_flag` in each of
+    the given `fill_cols`
     """
     for col in fill_cols:
         if col not in df.columns:
@@ -45,18 +31,18 @@ def check(df: pl.DataFrame, fill_cols: list[str], fill_flag: str) -> pl.DataFram
             )
             .sort("column")
         )
-    else:
-        fill_col = fill_cols[0]
-        return (
-            df.select(fill_col)
-            .unpivot(variable_name="column", value_name="value")
-            .group_by("column")
-            .agg(
-                count=pl.col("value").filter(pl.col("value") == fill_flag).count(),
-                prop=pl.col("value").filter(pl.col("value") == fill_flag).count()
-                / pl.count(),
-            )
+
+    fill_col = fill_cols[0]
+    return (
+        df.select(fill_col)
+        .unpivot(variable_name="column", value_name="value")
+        .group_by("column")
+        .agg(
+            count=pl.col("value").filter(pl.col("value") == fill_flag).count(),
+            prop=pl.col("value").filter(pl.col("value") == fill_flag).count()
+            / pl.count(),
         )
+    )
 
 
 def impute_columns(
@@ -64,16 +50,15 @@ def impute_columns(
     fill_cols: list[str],
     fill_flag: str,
     fill_range: tuple[int, int],
-    col_type: PolarsNumericType | None = None,
+    col_type: PolarsDataType = pl.Int64,
     seed: int | None = None,
 ) -> pl.DataFrame:
     """
-    Fill instances of the fill flag (a string) in the given column
+    Fill instances of `fill_flag` (a string) in the given column
     with random integers in the given range (inclusive).
 
-    If col_type is specified, will attempt to cast the final result
-    of fill_cols to that type. Currently, the only options are
-    Polars numeric types.
+    If `col_type` is specified, will attempt to cast the final result
+    of `fill_cols` to that Polars type.
     """
     for col in fill_cols:
         if col not in df.columns:
@@ -89,7 +74,7 @@ def impute_columns(
     if len(fill_cols) > 1:
         rng = np.random.default_rng(seed)
         n = (len(fill_cols), df.height)
-        # must gen all nums up front
+        # must gen enough numbers for whole column up-front, otherwise reused
         fill_nums = rng.integers(
             fill_range_int.lb,
             fill_range_int.ub,
@@ -103,46 +88,43 @@ def impute_columns(
                 .then(pl.lit(num))
                 .otherwise(pl.col(col))
                 .alias(col)
+                .cast(col_type)
             )
-
-            if col_type is not None:
-                df = df.with_columns(pl.col(col).cast(col_type))
     else:
         fill_col = fill_cols[0]
         # NOTE: this implementation and numpy implementation for filling values are roughly the same speed
-        # with this native impl barely faster
+        # with this Polars-only impl barely faster
         df = df.with_columns(
             pl.when(pl.col(fill_col) == fill_flag)
             .then(
                 pl.int_range(fill_range_int.lb, fill_range_int.ub + 1).sample(
-                    pl.len(),
-                    with_replacement=True,
-                    seed=seed,
+                    pl.len(), with_replacement=True, seed=seed
                 )
             )
             .otherwise(pl.col(fill_col))
-            .alias(fill_col),
+            .alias(fill_col)
+            .cast(col_type)
         )
-
-        if col_type is not None:
-            df = df.with_columns(pl.col(fill_col).cast(col_type))
 
     return df
 
 
-def impute_columns_lazy(
+def _impute_columns_lazy(
     lf: pl.DataFrame | pl.LazyFrame,
     fill_column: str,
     fill_flag: str,
     fill_range: tuple[int, int],
     seed: int | None = None,
 ) -> pl.DataFrame:
+    """
+    Experimental with Lazy
+    """
     lf = lf.lazy()
 
     if fill_column not in lf.collect_schema().names():
         raise ValueError(f"Column {fill_column} doesn't exist")
 
-    if not fill_flag_exists(lf, fill_column, fill_flag):
+    if not _fill_flag_exists_lazy(lf, fill_column, fill_flag):
         raise ValueError(
             f"Column {fill_column} doesn't contain any instances of '{fill_flag}'"
         )
@@ -153,7 +135,7 @@ def impute_columns_lazy(
         pl.when(pl.col(fill_column) == fill_flag)
         .then(
             pl.int_range(fill_range_int.lb, fill_range_int.ub + 1).sample(
-                pl.len(), with_replacement=True
+                pl.len(), with_replacement=True, seed=seed
             )
         )
         .otherwise(pl.col(fill_column))
@@ -163,15 +145,15 @@ def impute_columns_lazy(
     return df
 
 
-def fill_flag_exists(
+def fill_flag_exists(df: pl.DataFrame, fill_col: str, fill_flag: str) -> bool:
+    return df.select((pl.col(fill_col) == fill_flag).any()).item()
+
+
+def _fill_flag_exists_lazy(
     df: pl.DataFrame | pl.LazyFrame, fill_col: str, fill_flag: str
 ) -> bool:
-    # TODO: could just do lf = df.lazy() then don't need isinstance()
-    if isinstance(df, pl.DataFrame):
-        return df.select((pl.col(fill_col) == fill_flag).any()).item()
-    else:
-        # TODO: is there another way to check that doesn't materialize lf?
-        return df.select((pl.col(fill_col) == fill_flag).any()).collect().item()
+    lf = df.lazy()
+    return lf.select((pl.col(fill_col) == fill_flag).any()).collect().item()
 
 
 class FillRange(NamedTuple):
@@ -190,7 +172,8 @@ def parse_fill_range(fill_range: tuple[int, int]) -> FillRange:
     return fill_range_int
 
 
-# TODO: difficult to benchmark without reasonably sized data
+# TODO: instead of relying on .unique(), can allow user-defined mapping from col name
+# to list of possible values?
 def complete_rows(df: pl.DataFrame, columns: list[str]) -> pl.DataFrame:
     """
     Generate implicit missing rows based on the unique combinations
@@ -201,10 +184,11 @@ def complete_rows(df: pl.DataFrame, columns: list[str]) -> pl.DataFrame:
         df_expand = df_expand.explode(col)
 
     df = df_expand.join(df, on=columns, how="left", coalesce=True)
+
     return df
 
 
-def complete_rows_lazy(
+def _complete_rows_lazy(
     lf: pl.DataFrame | pl.LazyFrame, columns: list[str]
 ) -> pl.DataFrame:
     lf = lf.lazy()
@@ -223,87 +207,79 @@ def impute_column_pair(
     denominator: str,
     fill_flag: str,
     fill_range: tuple[int, int],
+    col_type: PolarsDataType = pl.Int64,
     seed: int | None = None,
 ) -> pl.DataFrame:
+    """
+    Fill instances of the fill_flag in both the numerator column
+    and the denominator column such that numerator <= denominator.
+
+    If col_type is specified, will attempt to cast the final result
+    of fill_cols to that type. Currently, the only options are
+    Polars numeric types.
+    """
+    # TODO: this should also handle denom being in another file like the CLI
+    # command?
+
+    if numerator not in df.columns:
+        raise ValueError(f"Column {numerator} doesn't exist")
+
+    if denominator not in df.columns:
+        raise ValueError(f"Column {numerator} doesn't exist")
+
+    if not fill_flag_exists(df, numerator, fill_flag):
+        raise ValueError(
+            f"Column {numerator} doesn't contain any instances of '{fill_flag}'"
+        )
+
+    if not fill_flag_exists(df, denominator, fill_flag):
+        raise ValueError(
+            f"Column {denominator} doesn't contain any instances of '{fill_flag}'"
+        )
+
+    fill_range_int = parse_fill_range(fill_range)
+
+    # TODO: I think repeated use of the same seed is undesirable
+    df = df.with_columns(
+        pl.when(pl.col(denominator) == fill_flag)
+        .then(
+            pl.int_range(fill_range_int.lb, fill_range_int.ub + 1).sample(
+                pl.len(),
+                with_replacement=True,
+                seed=seed,
+            )
+        )
+        .otherwise(pl.col(denominator))
+        .alias(denominator)
+        .cast(col_type)
+    )
+
+    df = df.with_columns(
+        # TODO: use list b/c no arr.sample() what about struct perf?
+        pl.when(
+            (pl.col(numerator) == fill_flag)
+            & (pl.col(denominator) <= fill_range_int.ub)
+        )
+        .then(
+            pl.int_ranges(fill_range_int.lb, pl.col(denominator) + 1)
+            # TODO: use of seed?
+            .list.sample(1)
+            .explode()
+        )
+        .when(
+            (pl.col(numerator) == fill_flag) & (pl.col(denominator) > fill_range_int.ub)
+        )
+        .then(
+            pl.int_range(fill_range_int.lb, fill_range_int.ub + 1).sample(
+                # TODO: use of seed?
+                pl.len(),
+                with_replacement=True,
+                seed=seed,
+            )
+        )
+        .otherwise(pl.col(numerator))
+        .alias(numerator)
+        .cast(pl.Int64)
+    )
+
     return df
-
-
-if __name__ == "__main__":
-    import timeit
-
-    print("Benchmarking implementations...")
-
-    repeat = 2
-    number = 100_000
-
-    # NOTE: impute_flag somehow slower? For gen_rows, if instead of passing in a LazyFrame I
-    # take a DataFrame and convert it to LazyFrame inside the func, the times are more comparable
-    # Maybe I'm doing benchmark wrong?
-
-    setup = """
-import polars as pl
-from __main__ import impute_columns
-df = pl.read_csv('../../tests/data/test_impute_data.csv')
-    """
-    t_eager = timeit.repeat(
-        "impute_columns(df, ['cases'], '<=5', (1, 5))",
-        setup=setup,
-        repeat=repeat,
-        number=number,
-    )
-
-    setup = """
-import polars as pl
-from __main__ import impute_columns_lazy
-df = pl.scan_csv('../../tests/data/test_impute_data.csv')
-    """
-    t_lazy = timeit.repeat(
-        "impute_columns_lazy(df, ['cases'], '<=5', (1, 5))",
-        setup=setup,
-        repeat=repeat,
-        number=number,
-    )
-
-    print(f"Min. time of impute.impute_columns(): {min(t_eager)}")
-    print(f"Min. time of impute.impute_columns_lazy(): {min(t_lazy)}")
-
-#     setup = """
-# import polars as pl
-# from __main__ import complete_rows
-# df = pl.DataFrame(
-#     {
-#         "orig": ["France", "France", "UK", "UK", "Spain"],
-#         "dest": ["Japan", "Vietnam", "Japan", "China", "China"],
-#         "year": [2020, 2021, 2019, 2020, 2022],
-#         "value": [1, 2, 3, 4, 5],
-#     }
-# )
-#     """
-#     t_eager = timeit.repeat(
-#         "complete_rows(df, ['orig', 'dest', 'year'])",
-#         setup=setup,
-#         repeat=repeat,
-#         number=number,
-#     )
-#
-#     setup = """
-# import polars as pl
-# from __main__ import complete_rows_lazy
-# df = pl.LazyFrame(
-#     {
-#         "orig": ["France", "France", "UK", "UK", "Spain"],
-#         "dest": ["Japan", "Vietnam", "Japan", "China", "China"],
-#         "year": [2020, 2021, 2019, 2020, 2022],
-#         "value": [1, 2, 3, 4, 5],
-#     }
-# )
-#     """
-#     t_lazy = timeit.repeat(
-#         "complete_rows_lazy(df, ['orig', 'dest', 'year'])",
-#         setup=setup,
-#         repeat=repeat,
-#         number=number,
-#     )
-#
-#     print(f"Min. time of impute.complete_rows(): {min(t_eager)}")
-#     print(f"Min. time of impute.complete_rows_lazy(): {min(t_lazy)}")
