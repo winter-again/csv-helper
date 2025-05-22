affa793c8e7e2b182e2fffa38f388c0e38408306
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2025-05-22 18:35:07 -0400

    Testing nox over tox

---
+++ noxfile.py
@@ -0,0 +1,15 @@
+import nox
+
+nox.options.default_venv_backend = "uv"
+
+
+@nox.session(python=["3.12", "3.13"])
+def tests(session):
+    """Run tests with pytest."""
+    session.run(
+        "uv",
+        "sync",
+        f"--python={session.virtualenv.location}",
+        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+    )
+    session.run("pytest", "-vv", "tests")
