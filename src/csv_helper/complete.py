3de7bb1f7fd63b4368c7ace6f3a9072122795130
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2025-05-22 09:57:17 -0400

    Reorganize modules. Clean up remaining funcs.

---
+++ src/csv_helper/complete.py
@@ -0,0 +1,34 @@
+import polars as pl
+
+
+def complete[T: (pl.DataFrame, pl.LazyFrame)](df: T, *columns: str | pl.Series) -> T:
+    """
+    Generate rows for implicit missing values based on column combinations,
+    thus making them explicit missing values. Generated values marked as null.
+
+    If columns are referenced with strings, then only existing values in those
+    columns are used for completion. If Series are specified instead, then
+    those Series can specify the full set of possible values, provided that
+    the Series is named after an existing column.
+    """
+    cols = []
+    for col in columns:
+        if isinstance(col, str):
+            cols.append(pl.col(col).unique().implode())
+        elif isinstance(col, pl.Series):
+            cols.append(col.unique().implode())
+        else:
+            raise TypeError(
+                f"The columns argument(s) must be either string or polars Series. Got {type(col)} instead."
+            )
+
+    unique_combos = df.select(cols)
+    col_names = unique_combos.collect_schema().names()
+    for col in col_names:
+        unique_combos = unique_combos.explode(col)
+
+    res = unique_combos.join(
+        df, on=col_names, how="left", coalesce=True, validate="1:1"
+    )
+
+    return res
--- src/csv_helper/impute.py
+++ src/csv_helper/impute.py
@@ -5,33 +5,25 @@ import polars as pl
 from polars.datatypes.classes import FloatType, IntegerType
 
 
-def foo(
-    df: pl.DataFrame,
-    column: str,
-    dtype: type[IntegerType] | type[FloatType] = pl.Int64,
-) -> pl.DataFrame:
-    return df.with_columns(pl.col(column).cast(dtype))
-
-
-# TODO: make this a check func that returns bool instead?
-# and separate this into another func like summarize()?
-def check(df: pl.DataFrame, fill_cols: list[str], fill_flag: str) -> pl.DataFrame:
+def check[T: (pl.DataFrame, pl.LazyFrame)](
+    df: T, columns: list[str], fill_flag: str
+) -> T:
     """
-    Return dataframe with counts and proportion of instances of `fill_flag` in each of
-    the given `fill_cols`
+    Summarize counts and proportion of instances of `fill_flag` in each of
+    the given columns.
     """
-    for col in fill_cols:
+    for col in columns:
         if col not in df.columns:
             raise ValueError(f"Column {col} doesn't exist")
 
-        if not fill_flag_exists(df, col, fill_flag):
+        if not _fill_flag_exists(df, col, fill_flag):
             raise ValueError(
                 f"Column {col} doesn't contain any instances of '{fill_flag}'"
             )
 
-    if len(fill_cols) > 1:
+    if len(columns) > 1:
         return (
-            df.select(fill_cols)
+            df.select(columns)
             .unpivot(variable_name="column", value_name="value")
             .group_by("column")
             .agg(
@@ -42,7 +34,8 @@ def check(df: pl.DataFrame, fill_cols: list[str], fill_flag: str) -> pl.DataFram
             .sort("column")
         )
 
-    fill_col = fill_cols[0]
+    fill_col = columns[0]
+
     return (
         df.select(fill_col)
         .unpivot(variable_name="column", value_name="value")
@@ -75,12 +68,12 @@ def columns[T: (pl.DataFrame, pl.LazyFrame)](
         if col not in df.columns:
             raise ValueError(f"Column {col} doesn't exist")
 
-        if not fill_flag_exists(df, col, fill_flag):
+        if not _fill_flag_exists(df, col, fill_flag):
             raise ValueError(
                 f"Column {col} doesn't contain any instances of '{fill_flag}'"
             )
 
-    fill_range_int = parse_fill_range(fill_range)
+    fill_range_int = _parse_fill_range(fill_range)
 
     n_cols = len(columns)
     if n_cols > 1:
@@ -122,22 +115,22 @@ def columns[T: (pl.DataFrame, pl.LazyFrame)](
     return df
 
 
-def fill_flag_exists[T: (pl.DataFrame, pl.LazyFrame)](
+def _fill_flag_exists[T: (pl.DataFrame, pl.LazyFrame)](
     df: T, column: str, fill_flag: str
 ) -> bool:
     return df.lazy().select((pl.col(column) == fill_flag).any()).collect().item()
 
 
-class FillRange(NamedTuple):
+class _FillRange(NamedTuple):
     lb: int
     ub: int
 
 
-def parse_fill_range(fill_range: tuple[int, int]) -> FillRange:
+def _parse_fill_range(fill_range: tuple[int, int]) -> _FillRange:
     if len(fill_range) != 2:
         raise ValueError("Must only pass 2 values")
 
-    fill_range_int = FillRange(*fill_range)
+    fill_range_int = _FillRange(*fill_range)
     if fill_range_int.lb > fill_range_int.ub:
         raise ValueError("Lower bound can't be greater than the upper bound")
 
@@ -172,17 +165,17 @@ def column_pair[T: (pl.DataFrame, pl.LazyFrame)](
     if denominator not in df.columns:
         raise ValueError(f"Column {numerator} doesn't exist")
 
-    if not fill_flag_exists(df, numerator, fill_flag):
+    if not _fill_flag_exists(df, numerator, fill_flag):
         raise ValueError(
             f"Column {numerator} doesn't contain any instances of '{fill_flag}'"
         )
 
-    if not fill_flag_exists(df, denominator, fill_flag):
+    if not _fill_flag_exists(df, denominator, fill_flag):
         raise ValueError(
             f"Column {denominator} doesn't contain any instances of '{fill_flag}'"
         )
 
-    fill_range_int = parse_fill_range(fill_range)
+    fill_range_int = _parse_fill_range(fill_range)
 
     df = df.with_columns(
         pl.when(pl.col(denominator) == fill_flag)
@@ -198,7 +191,7 @@ def column_pair[T: (pl.DataFrame, pl.LazyFrame)](
         .cast(dtype)
     ).with_columns(
         # TODO: use list b/c no arr.sample() what about struct perf?
-        # TODO: look into high mem consumption b/c of pl.int_ranges()
+        # NOTE: high mem consumption b/c of pl.int_ranges(), but not sure how to improve
         pl.when(
             (pl.col(numerator) == fill_flag)
             & (pl.col(denominator) <= fill_range_int.ub)
@@ -224,34 +217,3 @@ def column_pair[T: (pl.DataFrame, pl.LazyFrame)](
     )
 
     return df
-
-
-def complete[T: (pl.DataFrame, pl.LazyFrame)](df: T, *columns: str | pl.Series) -> T:
-    """
-    Generate rows for implicit missing values based on column combinations,
-    thus making them explicit missing values. Generated values marked as null.
-
-    If columns are referenced with strings, then only existing values in those
-    columns are used for completion. If Series are specified instead, then
-    those Series can specify the full set of possible values, provided that
-    the Series is named after an existing column.
-    """
-    cols = []
-    for col in columns:
-        if isinstance(col, str):
-            cols.append(pl.col(col).unique().implode())
-        elif isinstance(col, pl.Series):
-            cols.append(col.unique().implode())
-        else:
-            raise TypeError(
-                f"The columns argument(s) must be either string or polars Series. Got {type(col)} instead."
-            )
-
-    unique_combos = df.select(cols)
-    col_names = unique_combos.collect_schema().names()
-    for col in col_names:
-        unique_combos = unique_combos.explode(col)
-
-    return unique_combos.join(
-        df, on=col_names, how="left", coalesce=True, validate="1:1"
-    )
