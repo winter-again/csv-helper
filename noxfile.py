d2916c22f2ea9580282110eaa91fb2324172c492
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2025-05-23 08:54:56 -0400

    Need to run uv sync before nox?

--- noxfile.py
+++ noxfile.py
@@ -6,9 +6,11 @@ nox.options.default_venv_backend = "uv"
 @nox.session(python=["3.12", "3.13"])
 def tests(session: nox.Session) -> None:
     """Run tests with pytest."""
-    session.run(
+    # TODO: run_install vs run? needs install step...
+    session.run_install(
         "uv",
         "sync",
+        "--locked",
         f"--python={session.virtualenv.location}",
         env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
     )
