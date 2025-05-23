9aa37f80484f83c1d8f766241d1c5e66a05c5ad0
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2025-05-23 10:08:37 -0400

    Try without python versions in noxfile

--- noxfile.py
+++ noxfile.py
@@ -3,7 +3,7 @@ import nox
 nox.options.default_venv_backend = "uv"
 
 
-@nox.session(python=["3.12", "3.13"])
+@nox.session
 def tests(session: nox.Session) -> None:
     """Run tests with pytest."""
     session.run_install(
