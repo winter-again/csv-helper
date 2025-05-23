38bd6fad55326ddc62b3ce1e50ec0f69f5bc2f52
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2025-05-23 09:37:14 -0400

    Try again

--- noxfile.py
+++ noxfile.py
@@ -6,7 +6,6 @@ nox.options.default_venv_backend = "uv"
 @nox.session(python=["3.12", "3.13"])
 def tests(session: nox.Session) -> None:
     """Run tests with pytest."""
-    # TODO: run_install vs run? needs install step...
     session.run_install(
         "uv",
         "sync",
