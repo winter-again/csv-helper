292d4daf356fb11947d11aeb37ea0ae1c6a1a4d0
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2025-05-23 08:13:49 -0400

    Add uv sync inside of nox session back

--- noxfile.py
+++ noxfile.py
@@ -6,10 +6,10 @@ nox.options.default_venv_backend = "uv"
 @nox.session(python=["3.12", "3.13"])
 def tests(session: nox.Session) -> None:
     """Run tests with pytest."""
-    # session.run(
-    #     "uv",
-    #     "sync",
-    #     f"--python={session.virtualenv.location}",
-    #     env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
-    # )
+    session.run(
+        "uv",
+        "sync",
+        f"--python={session.virtualenv.location}",
+        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+    )
     session.run("pytest", "-vv", "tests")
