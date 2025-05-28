cc8ef6fa15195d74703d7338dc992e76b369fc70
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2025-05-28 15:34:27 -0400

    Use lazy() in some cases. Tests for LazyFrames.

--- tests/impute_test.py
+++ tests/impute_test.py
@@ -56,6 +56,55 @@ def df_inp() -> pl.DataFrame:
     return df
 
 
+@pytest.fixture
+def lf_inp() -> pl.LazyFrame:
+    data = """
+    id,numerator,denominator,imp_num,imp_denom
+    A,10,15,false,false
+    A,<=5,<=5,true,true
+    A,12,23,false,false
+    B,<=5,<=5,true,true
+    A,22,24,false,false
+    B,<=5,13,true,false
+    B,<=5,<=5,true,true
+    A,10,15,false,false
+    C,<=5,<=5,false,true
+    C,<=5,<=5,true,true
+    A,<=5,<=5,true,true
+    A,22,15,false,false
+    B,<=5,13,true,false
+    A,<=5,<=5,false,true
+    C,100,128,false,false
+    C,<=5,<=5,true,true
+    D,<=5,<=5,true,true
+    A,22,23,false,false
+    B,<=5,18,true,false
+    H,8,17,false,false
+    A,10,16,false,false
+    A,<=5,<=5,true,true
+    H,<=5,<=5,true,true
+    A,22,88,false,false
+    B,<=5,23,true,false
+    C,<=5,<=5,true,true
+    A,<=5,<=5,false,true
+    C,100,1300,false,false
+    C,<=5,<=5,true,true
+    D,<=5,<=5,true,true
+    """
+    lf = pl.scan_csv(
+        StringIO(data),
+        schema={
+            "id": pl.String,
+            "numerator": pl.String,
+            "denominator": pl.String,
+            "imp_num": pl.Boolean,
+            "imp_denom": pl.Boolean,
+        },
+    )
+
+    return lf
+
+
 def test_impute_columns_single(df_inp: pl.DataFrame) -> None:
     df_out = df_inp.pipe(impute.columns, ["numerator"], "<=5", (1, 5))
 
@@ -66,6 +115,25 @@ def test_impute_columns_single(df_inp: pl.DataFrame) -> None:
     )
     assert (
         df_out.filter(pl.col("imp_num")).select((pl.col("numerator") <= 5).all()).item()
+        is True
+    )
+
+
+def test_impute_columns_single_lazy(lf_inp: pl.LazyFrame) -> None:
+    lf_out = lf_inp.pipe(impute.columns, ["numerator"], "<=5", (1, 5))
+
+    assert lf_inp.collect().shape == lf_out.collect().shape
+    assert (
+        lf_out.select((pl.col("numerator").cast(pl.String) == "<=5").any())
+        .collect()
+        .item()
+        is False
+    )
+    assert (
+        lf_out.filter(pl.col("imp_num"))
+        .select((pl.col("numerator") <= 5).all())
+        .collect()
+        .item()
         is True
     )
 
@@ -95,6 +163,39 @@ def test_impute_columns_multi(df_inp: pl.DataFrame) -> None:
     )
 
 
+def test_impute_columns_multi_lazy(lf_inp: pl.LazyFrame) -> None:
+    lf_out = lf_inp.pipe(impute.columns, ["numerator", "denominator"], "<=5", (1, 5))
+
+    assert lf_inp.collect().shape == lf_out.collect().shape
+    assert (
+        lf_out.select((pl.col("numerator").cast(pl.String) == "<=5").any())
+        .collect()
+        .item()
+        is False
+    )
+    assert (
+        lf_out.select((pl.col("denominator").cast(pl.String) == "<=5").any())
+        .collect()
+        .item()
+        is False
+    )
+
+    assert (
+        lf_out.filter(pl.col("imp_num"))
+        .select((pl.col("numerator") <= 5).all())
+        .collect()
+        .item()
+        is True
+    )
+    assert (
+        lf_out.filter(pl.col("imp_denom"))
+        .select((pl.col("denominator") <= 5).all())
+        .collect()
+        .item()
+        is True
+    )
+
+
 def test_impute_columns_seed(df_inp: pl.DataFrame) -> None:
     df_1 = df_inp.pipe(impute.columns, ["numerator"], "<=5", (1, 5))
     df_2 = df_inp.pipe(impute.columns, ["numerator"], "<=5", (1, 5))
@@ -107,6 +208,20 @@ def test_impute_columns_seed(df_inp: pl.DataFrame) -> None:
 
     assert df_1.shape == df_2.shape
     assert_frame_equal(df_1, df_2)
+
+
+def test_impute_columns_seed_lazy(lf_inp: pl.LazyFrame) -> None:
+    lf_1 = lf_inp.pipe(impute.columns, ["numerator"], "<=5", (1, 5))
+    lf_2 = lf_inp.pipe(impute.columns, ["numerator"], "<=5", (1, 5))
+
+    assert lf_1.collect().shape == lf_2.collect().shape
+    assert_frame_not_equal(lf_1, lf_2)
+
+    lf_1 = lf_inp.pipe(impute.columns, ["numerator"], "<=5", (1, 5), seed=18)
+    lf_2 = lf_inp.pipe(impute.columns, ["numerator"], "<=5", (1, 5), seed=18)
+
+    assert lf_1.collect().shape == lf_2.collect().shape
+    assert_frame_equal(lf_1, lf_2)
 
 
 def test_impute_pair(df_inp: pl.DataFrame) -> None:
@@ -133,6 +248,44 @@ def test_impute_pair(df_inp: pl.DataFrame) -> None:
     assert (
         df_out.filter(pl.col("imp_denom"))
         .select((pl.col("numerator") <= pl.col("denominator")).all())
+        .item()
+        is True
+    )
+
+
+def test_impute_pair_lazy(lf_inp: pl.LazyFrame) -> None:
+    lf_out = lf_inp.pipe(impute.column_pair, "numerator", "denominator", "<=5", (1, 5))
+
+    assert lf_inp.collect().shape == lf_out.collect().shape
+    assert (
+        lf_out.select((pl.col("numerator").cast(pl.String) == "<=5").any())
+        .collect()
+        .item()
+        is False
+        and lf_out.select((pl.col("denominator").cast(pl.String) == "<=5").any())
+        .collect()
+        .item()
+        is False
+    )
+    assert (
+        lf_out.filter(pl.col("imp_num"))
+        .select((pl.col("numerator") <= 5).all())
+        .collect()
+        .item()
+        is True
+    )
+    assert (
+        lf_out.filter(pl.col("imp_denom"))
+        .select((pl.col("denominator") <= 5).all())
+        .collect()
+        .item()
+        is True
+    )
+
+    assert (
+        lf_out.filter(pl.col("imp_denom"))
+        .select((pl.col("numerator") <= pl.col("denominator")).all())
+        .collect()
         .item()
         is True
     )
