76cdb1e439b710c9b782d9095f232fd75dc32e34
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2025-06-21 08:18:22 -0400

    Clean up complete tests

--- tests/complete_test.py
+++ tests/complete_test.py
@@ -25,101 +25,88 @@ def df_inp() -> pl.DataFrame:
             "year": pl.Int64,
             "value": pl.Int64,
         },
-    )
+    ).sort("country", "year")
 
     return df
 
 
 @pytest.fixture
-def lf_inp() -> pl.LazyFrame:
+def lf_inp(df_inp: pl.DataFrame) -> pl.LazyFrame:
+    return df_inp.lazy()
+
+
+@pytest.fixture
+def df_out() -> pl.DataFrame:
     data = """\
     country,year,value
+    France,2019,
     France,2020,1
     France,2021,2
+    France,2022,
     UK,2019,3
     UK,2020,4
+    UK,2021,
+    UK,2022,
+    Spain,2019,
+    Spain,2020,
+    Spain,2021,
     Spain,2022,5
     """
-    lf = pl.scan_csv(
+    df = pl.read_csv(
         StringIO(textwrap.dedent(data)),
         schema={
             "country": pl.String,
             "year": pl.Int64,
             "value": pl.Int64,
         },
-    )
-
-    return lf
-
-
-def test_complete_existing(df_inp: pl.DataFrame) -> None:
-    df_out = df_inp.pipe(complete.complete, "country", "year").sort("country", "year")
-
-    data_res = """\
-    country,year,value
-    France,2019,
-    France,2020,1
-    France,2021,2
-    France,2022,
-    UK,2019,3
-    UK,2020,4
-    UK,2021,
-    UK,2022,
-    Spain,2019,
-    Spain,2020,
-    Spain,2021,
-    Spain,2022,5
-    """
-    result = pl.read_csv(
-        StringIO(textwrap.dedent(data_res)),
-        schema={
-            "country": pl.String,
-            "year": pl.Int64,
-            "value": pl.Int64,
-        },
     ).sort("country", "year")
 
-    assert_frame_equal(df_out, result)
+    return df
 
 
-def test_complete_existing_lazy(lf_inp: pl.LazyFrame) -> None:
-    lf_out = lf_inp.pipe(complete.complete, "country", "year").sort("country", "year")
+def test_complete_existing(df_inp: pl.DataFrame, df_out: pl.DataFrame) -> None:
+    df = df_inp.pipe(complete.complete, "country", "year").sort("country", "year")
 
-    data_res = """\
-    country,year,value
-    France,2019,
-    France,2020,1
-    France,2021,2
-    France,2022,
-    UK,2019,3
-    UK,2020,4
-    UK,2021,
-    UK,2022,
-    Spain,2019,
-    Spain,2020,
-    Spain,2021,
-    Spain,2022,5
-    """
-    result = pl.read_csv(
-        StringIO(textwrap.dedent(data_res)),
-        schema={
-            "country": pl.String,
-            "year": pl.Int64,
-            "value": pl.Int64,
-        },
-    ).sort("country", "year")
-
-    assert_frame_equal(lf_out.collect(), result)
+    assert_frame_equal(df, df_out)
 
 
-def test_complete_non_existing(df_inp: pl.DataFrame) -> None:
-    df_out = df_inp.pipe(
-        complete.complete,
-        pl.Series("country", ["France", "UK", "Spain", "China"]),
-        "year",
-    ).sort("country", "year")
+def test_complete_existing_series(df_inp: pl.DataFrame, df_out: pl.DataFrame) -> None:
+    country = pl.Series("country", ["France", "UK", "Spain"])
+    year = pl.Series("year", [year for year in range(2019, 2023)])
+    df = df_inp.pipe(complete.complete, country, year).sort("country", "year")
 
-    data_res = """\
+    assert_frame_equal(df, df_out)
+
+
+def test_complete_exception(df_inp: pl.DataFrame) -> None:
+    with pytest.raises(TypeError):
+        df_inp.pipe(complete.complete, 0, 1).sort("country", "year")  # pyright: ignore[reportArgumentType]
+
+
+def test_complete_existing_lazy(lf_inp: pl.LazyFrame, df_out: pl.DataFrame) -> None:
+    lf = lf_inp.pipe(complete.complete, "country", "year").sort("country", "year")
+
+    assert_frame_equal(lf.collect(), df_out)
+
+
+def test_complete_existing_lazy_series(
+    lf_inp: pl.LazyFrame, df_out: pl.DataFrame
+) -> None:
+    country = pl.Series("country", ["France", "UK", "Spain"])
+    year = pl.Series("year", [year for year in range(2019, 2023)])
+    lf = lf_inp.pipe(complete.complete, country, year).sort("country", "year")
+
+    assert_frame_equal(lf.collect(), df_out)
+
+
+def test_complete_exception_lazy(lf_inp: pl.LazyFrame) -> None:
+    with pytest.raises(TypeError):
+        lf_inp.pipe(complete.complete, 0, 1).sort("country", "year")  # pyright: ignore[reportArgumentType]
+
+
+@pytest.fixture
+def df_out_non_exist() -> pl.DataFrame:
+    data = """\
     country,year,value
     China,2019,
     China,2020,
@@ -138,8 +125,8 @@ def test_complete_non_existing(df_inp: pl.DataFrame) -> None:
     Spain,2021,
     Spain,2022,5
     """
-    result = pl.read_csv(
-        StringIO(textwrap.dedent(data_res)),
+    df = pl.read_csv(
+        StringIO(textwrap.dedent(data)),
         schema={
             "country": pl.String,
             "year": pl.Int64,
@@ -147,42 +134,28 @@ def test_complete_non_existing(df_inp: pl.DataFrame) -> None:
         },
     ).sort("country", "year")
 
-    assert_frame_equal(df_out, result)
+    return df
 
 
-def test_complete_non_existing_lazy(lf_inp: pl.LazyFrame) -> None:
-    lf_out = lf_inp.pipe(
+def test_complete_non_existing(
+    df_inp: pl.DataFrame, df_out_non_exist: pl.DataFrame
+) -> None:
+    df = df_inp.pipe(
         complete.complete,
         pl.Series("country", ["France", "UK", "Spain", "China"]),
         "year",
     ).sort("country", "year")
 
-    data_res = """\
-    country,year,value
-    China,2019,
-    China,2020,
-    China,2021,
-    China,2022,
-    France,2019,
-    France,2020,1
-    France,2021,2
-    France,2022,
-    UK,2019,3
-    UK,2020,4
-    UK,2021,
-    UK,2022,
-    Spain,2019,
-    Spain,2020,
-    Spain,2021,
-    Spain,2022,5
-    """
-    result = pl.read_csv(
-        StringIO(textwrap.dedent(data_res)),
-        schema={
-            "country": pl.String,
-            "year": pl.Int64,
-            "value": pl.Int64,
-        },
+    assert_frame_equal(df, df_out_non_exist)
+
+
+def test_complete_non_existing_lazy(
+    lf_inp: pl.LazyFrame, df_out_non_exist: pl.DataFrame
+) -> None:
+    lf = lf_inp.pipe(
+        complete.complete,
+        pl.Series("country", ["France", "UK", "Spain", "China"]),
+        "year",
     ).sort("country", "year")
 
-    assert_frame_equal(lf_out.collect(), result)
+    assert_frame_equal(lf.collect(), df_out_non_exist)
