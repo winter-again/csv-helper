8128c03c1895dbd37f305ed7004a4869718bbda3
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2025-06-21 08:18:22 -0400

    Clean up complete tests

--- src/csv_helper/complete.py
+++ src/csv_helper/complete.py
@@ -6,10 +6,10 @@ def complete[T: (pl.DataFrame, pl.LazyFrame)](df: T, *columns: str | pl.Series) 
     Generate rows for implicit missing values based on column combinations,
     thus making them explicit missing values. Generated values marked as null.
 
-    If columns are referenced with strings, then only existing values in those
+    If columns are referenced by name, then only existing values in those
     columns are used for completion. If Series are specified instead, then
-    those Series can specify the full set of possible values, provided that
-    the Series is named after an existing column.
+    those Series can specify the full set of possible values. The Series must be
+    named after an existing column.
     """
     cols = []
     for col in columns:
@@ -19,7 +19,7 @@ def complete[T: (pl.DataFrame, pl.LazyFrame)](df: T, *columns: str | pl.Series) 
             cols.append(col.unique().implode())
         else:
             raise TypeError(
-                f"The columns argument(s) must be either string or polars Series. Got {type(col)} instead."
+                f"The columns must be either string or polars Series. Got {type(col)} instead."
             )
 
     unique_combos = df.select(cols)
